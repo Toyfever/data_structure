d2fbbc11c5b4f870889eabcd58353de50eaf497b
Antoine de Fourcroy <antoine.defourcroy@gmail.com>
2022-02-14 16:36:40 +0100

    Checks if weight is valid

--- pounds_converter.py
+++ pounds_converter.py
@@ -4,7 +4,17 @@ POUND_TO_KILO = 0.453592
 
 def main():
     value = input('Please select your measuring value in Kilos (K) or Pounds (P): ').lower()
-    weight = float(input('Please enter the weight to convert (numbers only): '))
+    valid_weight = False
+    while not valid_weight:
+        weight = input('Please enter the weight to convert (numbers only): ')
+        try:
+            weight = float(weight)
+            valid_weight = True
+            break
+        except ValueError:
+            print(ValueError('Weight must be a number'))
+            continue
+
     if value == 'p':
         result = convert_pounds_to_kilos(weight)
         print(f'The weight of {weight} pounds is equal to {round(result, 2)} kilos.')
@@ -12,7 +22,7 @@ def main():
         result = convert_kilos_to_pounds(weight)
         print(f'The weight of {weight} kilos is equal to {round(result, 2)} pounds.')
     else:
-        print('Could not find the value you entered, terminating program.')
+        print('Could not find the conversion value you entered, closing program.')
 
 
 def convert_pounds_to_kilos(weight):
